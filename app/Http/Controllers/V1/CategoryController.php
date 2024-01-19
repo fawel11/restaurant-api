8155c94bc1df713c27c1f2b81792ea90ae09859e
Nur <fawel99@gmail.com>
2024-01-19 12:12:17 +0600

    Added category resource

--- app/Http/Controllers/V1/CategoryController.php
+++ app/Http/Controllers/V1/CategoryController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 
 class CategoryController extends Controller
 {
+
+    public function index(CategoryRequest $request)
+    {
+        $category = Category::all();
+        return CategoryResource::collection($category);
+    }
+
     public function store(CategoryRequest $request)
     {
         $category = Category::create($request->all());
