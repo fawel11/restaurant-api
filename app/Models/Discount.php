b4d44b054a022dc5ef2cd14c9fa9dd1c0cec0233
Nur <fawel99@gmail.com>
2024-01-19 09:16:35 +0600

    Added Discount model with relation

---
+++ app/Models/Discount.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Discount extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['type', 'value', 'category_id', 'item_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+}
