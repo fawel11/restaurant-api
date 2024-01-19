7230f87215aa665945c389216e653b4e48dbd830
Nur <fawel99@gmail.com>
2024-01-19 09:15:28 +0600

    Added Item model

---
+++ app/Models/Item.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'amount', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+}
