9c2e3e53aef9d8eaabf621775654e629e480cc55
Nur <fawel99@gmail.com>
2024-01-20 20:44:04 +0600

    New mutator file added as eloquent mutator not working

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Log;
 
 class Discount extends Model
 {
@@ -20,5 +21,18 @@ class Discount extends Model
     public function item(): BelongsTo
     {
         return $this->belongsTo(Item::class);
+    }
+
+    public function setTypeAttribute($value)
+    {
+        // Somehow this mutator is not mutating -- need to debug more
+
+        if ($this->getAttribute('category_id') !== null) {
+            $this->attributes['type'] = 'category';
+        } elseif ($this->getAttribute('item_id') !== null) {
+            $this->attributes['type'] = 'item';
+        } else {
+            $this->attributes['type'] = 'all_menu';
+        }
     }
 }
--- app/Mutators/TypeMutator.php
+++ app/Mutators/TypeMutator.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace App\Mutators;
+
+class TypeMutator
+{
+    public function mutateType($discount)
+    {
+
+        if ($discount->category_id !== null) {
+            return 'category';
+        } elseif ($discount->item_id !== null) {
+            return 'item';
+        } else {
+            return 'all_menu';
+        }
+    }
+}
