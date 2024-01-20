4e48334ec59187bbce05d861f472b0bcf4b8c6d6
Nur <fawel99@gmail.com>
2024-01-20 14:41:18 +0600

    Modified return message for store method

--- app/Http/Controllers/V1/CategoryController.php
+++ app/Http/Controllers/V1/CategoryController.php
@@ -6,19 +6,37 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CategoryController extends Controller
 {
+    protected $category;
 
-    public function index(CategoryRequest $request)
+    public function __construct(Category $category)
     {
-        $category = Category::all();
+        $this->category = $category;
+
+    }
+
+    public function index(Request $request)
+    {
+        Log::info($request);
+        $categoryId = $request->categoryId ?? null;
+        $paginate = $request->paginate;
+        $category = $this->category
+            ->with('children', 'items')
+            ->whereParentId($categoryId)
+            ->paginate($paginate);
+
         return CategoryResource::collection($category);
     }
 
     public function store(CategoryRequest $request)
     {
+        //$validatedData = $request->validated();
+
         $category = Category::create($request->all());
-        return response()->json($category, 201);
+        return response()->json(['message' => 'Category added successfully!'], 201);
     }
 }
