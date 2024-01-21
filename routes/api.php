02e30c9401322f9400beaa06ae8063650000844e
Nur <fawel99@gmail.com>
2024-01-21 09:34:47 +0600

    Added name to all routes

--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,11 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1'], function () {
     Route::group(['prefix' => 'auth'], function () {
         Route::post('login', [AuthController::class, 'login'])->name('login');;
-        Route::post('register', [AuthController::class, 'register']);
+        Route::post('register', [AuthController::class, 'register'])->name('register');
 
         Route::group(['middleware' => 'auth:sanctum'], function () {
-            Route::get('logout', [AuthController::class, 'logout']);
-            Route::get('user', [AuthController::class, 'user']);
+            Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+            Route::get('user', [AuthController::class, 'user'])->name('user');
         });
 
     });
@@ -35,18 +35,18 @@ Route::group(['prefix' => 'v1'], function () {
     Route::group(['namespace' => 'V1', 'middleware' => 'auth:sanctum'], function () {
 
         Route::group(['prefix' => 'category'], function () {
-            Route::get('list', [CategoryController::class, 'index']);
-            Route::post('store', [CategoryController::class, 'store']);
+            Route::get('list', [CategoryController::class, 'index'])->name('category.index');
+            Route::post('store', [CategoryController::class, 'store'])->name('category.store');
         });
 
         Route::group(['prefix' => 'item'], function () {
-            Route::get('list', [ItemController::class, 'index']);
-            Route::post('store', [ItemController::class, 'store']);
+            Route::get('list', [ItemController::class, 'index'])->name('item.index');
+            Route::post('store', [ItemController::class, 'store'])->name('item.store');
         });
 
         Route::group(['prefix' => 'discount'], function () {
-            Route::get('list', [DiscountController::class, 'index']);
-            Route::post('store', [DiscountController::class, 'store']);
+            Route::get('list', [DiscountController::class, 'index'])->name('discount.index');
+            Route::post('store', [DiscountController::class, 'store'])->name('discount.store');
         });
 
         Route::group(['prefix' => 'menu'], function () {
